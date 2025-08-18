d0a859dbffc7b164517652f704346c11c8a1b78c
Sam Dams <sam_dams@hotmail.com>
2025-08-18 07:47:49 +0200

    commit 3

--- Les/src/main.tsx
+++ Les/src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import HomePage from './HomePage';
+import Navbar from './Navbar';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Navbar />
+    <div className="container py-5">
+    <HomePage />
+      </div>
   </StrictMode>,
 )
